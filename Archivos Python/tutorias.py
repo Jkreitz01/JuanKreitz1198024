3fc7da7bc3fb70915a5ec1c01d083032e17b2ad8
Jkreitz01 <158313432+Jkreitz01@users.noreply.github.com>
2024-05-21 21:46:57 -0600

    Proyecto 2

--- Archivos Python/tutorias.py
+++ Archivos Python/tutorias.py
@@ -1,33 +1,62 @@
-import random
-
-m = int(input("Ingrese la cantidad de filas: "))
-n = int(input("Ingrese la cantidad de columnas: "))
-
-matriz = []
-for i in range(m):
-    arreglo = [] 
-    for j in range(n):
-        dato = random.randint(-20, 20)
-        arreglo.append(dato)  
-    matriz.append(arreglo) 
-
-print("Matriz original:")
-for i in range(m):
-    for j in range(n):
-        print(matriz[i][j], end=" ")  
-    print() 
-
-matriz_traspuesta = []
-for j in range(n):
-    arreglo = []
-    for i in range(m):
-        arreglo.append(matriz[i][j])
-    matriz_traspuesta.append(arreglo)
-
-print("\nMatriz traspuesta:")
-for i in range(n):
-    for j in range(m):
-        print(matriz_traspuesta[i][j], end=" ")  
-    print() 
-
-
+n=int(input("Ingrese la cantidad de piezas en el tablero: "))
+Tabla = [['-' for _ in range(1,8)] for _ in range(1,8)]
+def datos_piezas():
+    for i in range(0,n):
+        print("1) Rey", "2) Reina", "3) Arfil", "4) Caballo", "5) Peón", sep="\n")
+        p=(input("Ingrese el tipo de pieza que desea: "))
+        c=(input("¿La pieza será blanco o negro? "))
+        if c!='blanco' or c!='negro':
+            print("Ingrese un color valido")  
+        e=input("Ingrese la columna de ubicación (a-h): ")
+        if e=='a':
+            col=1
+        elif e=='b':
+            col=2
+        elif e=='c':
+            col=3
+        elif e=='d':
+            col=4
+        elif e=='e':
+            col=5
+        elif e=='f':
+            col=6
+        elif e=='g':
+            col=7
+        elif e=='h':
+            col=8
+        else:
+            print("Ingrese una columna valida")
+        f=int(input("Ingrese la fila de ubicación (1-8): "))
+        if 1>f>8:
+            print("ingrese una fila valida")
+        Tabla[f][col]=p,c
+#Sintaxis
+datos_piezas()
+#datos de la torre
+ct=(input("¿La torre será blanco o negro? "))
+if ct!="blanco" or ct!="negro":
+    print("Ingrese un color valido")    
+et=input("Ingrese la columna de ubicación (a-h): ")
+if et=='a':
+    colt=1
+elif et=='b':
+    colt=2
+elif et=='c':
+    colt=3
+elif et=='d':
+    colt=4
+elif et=='e':
+    colt=5
+elif et=='f':
+    colt=6
+elif et=='g':
+    colt=7
+elif et=='h':
+    colt=8
+else:
+    print("Ingrese una columna valida")
+ft=int(input("Ingrese la fila de ubicación (1-8): "))
+if 1>ft>8:
+    print("ingrese una fila valida: ")
+Tabla[ft][colt]="Torre", ct
+print(Tabla)
